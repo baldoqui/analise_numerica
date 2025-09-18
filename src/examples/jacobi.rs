001326c9b0eb6fe8d01567e69ec9b713a7974db3
Arthur Baldoqui <56744257+baldoqui@users.noreply.github.com>
2025-09-18 13:21:11 -0300

    refactor(example): decouple Jacobi example from main (#3)

---
+++ src/examples/jacobi.rs
@@ -0,0 +1,29 @@
+use crate::iterative_methods::jacobi::jacobi_iteration;
+use log::{info, error};
+
+pub fn jacobi_example() {
+    let a = vec![
+        vec![10.0, -1.0, 2.0, 0.0],
+        vec![-1.0, 11.0, -1.0, 3.0],
+        vec![2.0, -1.0, 10.0, -1.0],
+        vec![0.0, 3.0, -1.0, 8.0],
+    ];
+    let b = vec![6.0, 25.0, -11.0, 11.0];
+    let x0 = vec![0.0, 0.0, 0.0, 0.0];
+    let tol = 1e-5;
+    let max_iter = 100;
+
+    info!("Resolvendo o sistema Ax=b com a método de Jacobi");
+    info!("Matrix A: {:?}", a);
+    info!("Vetor b: {:?}", b);
+    info!("Tolerância: {}, Máx. Iterações: {}\n", tol, max_iter);
+
+    match jacobi_iteration(&a, &b, &x0, tol, max_iter) {
+        Ok(x) => {
+            info!("Solução aproximada: {:?}", x);
+        }
+        Err(e) => {
+            error!("Erro: {}", e);
+        }
+    }
+}
