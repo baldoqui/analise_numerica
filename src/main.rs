472d7baf813d2c5e413f6226775b25b1875fbbfa
Arthur Baldoqui <arthur.b.bergamo@gmail.com>
2025-09-18 13:20:23 -0300

    refactor(example): decouple Jacobi example from main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod iterative_methods;
 mod cli;
+mod examples;
 
 use env_logger;
-use log::{info, error, LevelFilter};
+use log::{LevelFilter};
 use std::str::FromStr;
 use clap::Parser;
 
-use iterative_methods::jacobi::jacobi_iteration;
 use cli::Args;
+use examples::jacobi;
 
 fn main() {
     let args = Args::parse();
@@ -19,33 +20,5 @@ fn main() {
         .format_target(false)
         .init();
 
-
-    let a: Vec<Vec<f64>> = vec![
-        vec![10.0, -1.0, 2.0, 0.0],
-        vec![-1.0, 11.0, -1.0, 3.0],
-        vec![2.0, -1.0, 10.0, -1.0],
-        vec![0.0, 3.0, -1.0, 8.0],
-    ];
-    let b: Vec<f64> = vec![6.0, 25.0, -11.0, 15.0];
-    
-    let x0: Vec<f64> = vec![0.0, 0.0, 0.0, 0.0];
-    let tol: f64 = 1e-5;
-    let max_iter: usize = 100;
-
-    info!("Resolvendo o sistema Ax=b com a método de Jacobi");
-    info!("Matrix A: {:?}", a);
-    info!("Vetor b: {:?}", b);
-    info!("Tolerância: {}, Máx. Iterações: {}\n", tol, max_iter);
-
-    match jacobi_iteration(&a, &b, &x0, tol, max_iter) {
-        Ok(solution) => {
-            info!("Solução convergiu:");
-            for (i, val) in solution.iter().enumerate() {
-                info!("  x[{}] = {:.6}", i, val);
-            }
-        }
-        Err(e) => {
-            error!("Erro: {}", e);
-        }
-    }
+    jacobi::jacobi_example();
 }
