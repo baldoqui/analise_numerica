ba1a84bd467c114b4b2c08095322c0b33ae46b0c
baldoqui <arthur.b.bergamo@gmail.com>
2025-09-25 17:47:00 -0300

    feat(subcommands): setup subcommands to run example

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,17 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     #[arg(long, default_value = "info")]
     pub log_level: String,
+
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Jacobi,
+    BinarySearch,
 }
