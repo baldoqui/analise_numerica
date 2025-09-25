f55be09ac48eb7e13025ee59ab66ca0769d250a1
Arthur Baldoqui <56744257+baldoqui@users.noreply.github.com>
2025-09-25 17:45:19 -0300

    feat(binary_search): create new method (#5)
    * feat(binary_search): implement method
    
    * feat(binary_search): implement example

---
+++ src/iterative_methods/binary_search.rs
@@ -0,0 +1,23 @@
+pub fn binary_search<F>(f: F, mut a: f64, mut b: f64, tol: f64, max_iter: usize) -> Result<f64, String>
+where
+    F: Fn(f64) -> f64,
+{
+    if f(a) * f(b) >= 0.0 {
+        return Err("A função deve ter sinais opostos nos pontos a e b.".to_string());
+    }
+
+    let mut iter = 0;
+    while (b - a) / 2.0 > tol && iter < max_iter {
+        let c = (a + b) / 2.0;
+        if f(c) == 0.0 {
+            return Ok(c);
+        } else if f(a) * f(c) < 0.0 {
+            b = c;
+        } else {
+            a = c;
+        }
+        iter += 1;
+    }
+
+    Ok((a + b) / 2.0)
+}
--- src/iterative_methods/mod.rs
+++ src/iterative_methods/mod.rs
@@ -1,1 +1,2 @@
 pub mod jacobi;
+pub mod binary_search;
